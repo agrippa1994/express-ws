e93ddc559e58cf2ed9e9724777a1bd9c2fbe00ef
agrippa1994 <agrippa1994@icloud.com>
2015-08-22 15:11:30 +0200

    It's now working (for me)

--- examples/simple.js
+++ examples/simple.js
@@ -4,22 +4,35 @@ var expressWs = require('..');
 var expressWs = expressWs(express());
 var app = expressWs.app;
 
-app.use(function (req, res, next) {
-  console.log('middleware');
-  req.testing = 'testing';
-  return next();
-});
-
-app.get('/', function(req, res, next){
-  console.log('get route', req.testing);
-  res.end();
+app.get('/', function(req, res) {
+	console.log('http route');
+	res.end();
 });
 
 app.ws('/', function(ws, req) {
-  ws.on('message', function(msg) {
-    console.log(msg);
-  });
-  console.log('socket', req.testing);
+
+	ws.on('message', function(msg) {
+		console.log("message '/': " + msg);
+	});
+
+	ws.on("close", function() {
+		console.log("ws '/' closed");
+	});
 });
 
-app.listen(3000)
+app.ws('/test', function(ws, req) {
+
+	ws.on('message', function(msg) {
+		console.log("message '/test': " + msg);
+
+		expressWs.clientsForRoute("/test").forEach(function(client) {
+			client.send('hello');
+		});
+	});
+
+	ws.on("close", function() {
+		console.log("ws '/test' closed");
+	});
+});
+
+app.listen(3000);
